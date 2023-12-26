812fa6f3d041ece39fbdaeecba9f456001cf99a9
jjhuang4 <jjhuang4@illinois.edu>
2023-12-26 01:11:47 -0600

    Testing with streamlit

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,19 @@ import pandas as pd
 import numpy as np
 import os
 
-st.write("""
-# Credit Fraud Detection
-*Hello there!*         
-""")
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+from sklearn.metrics import precision_recall_curve
+
+df = pd.read_csv("creditcard.csv")
+
+st.title("Credit Card Fraud Detection Model Benchmarking")
+scale = st.checkbox("Scale the data: ")
+test = st.radio("Choose options: ", ("Train", "Test"))
+select = st.selectbox("Options: ", ['A', 'B', 'C'])
+many_select = st.multiselect("Options: ", ['A', 'B', 'C'])
+txt = st.text_input("Enter some value here",  "Please enter ...")
+slide = st.slider("Select level", 1, 10)
+
